653d7dc7523b3e9e72a3772293a40b35db815bc2
Natalia Lukinykh <Natalia.Lukinykh@vitec.com>
2026-04-28 15:46:40 +0100

    Added test id for cards and created a new test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -6,3 +6,10 @@ test('Check header', async ({ page }) => {
   const header = page.getByTestId('homePageHeader');
   await expect(header).toBeVisible();
  });
+
+ test('Check card list items', async ({ page }) => {
+  await page.goto('');
+  const fisrtCard = page.getByTestId('catCard_0');
+  const cardListItems = page.getByTestId(/catCard/)
+  await expect(fisrtCard).toBeVisible();
+ });
